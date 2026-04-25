ad11afa0b6e301464b0d783a467cb6760a27b91a
Betzabe Pinon <NikkiPilk@outlook.com>
2026-04-25 00:33:40 -0500

    added two table EggSale and Retail and schema Demo Idk what is the offical name for it

---
+++ Sql/Schema/Demo.sql
@@ -0,0 +1,9 @@
+﻿IF Not exists
+(
+	Select *
+	From sys.schemas s
+	Where s.[name] = N'Demo'
+)
+Begin
+	Exec(N'CREATE SCHEMA [Demo] AUTHORIZATION [dbo]')
+End;
---
+++ Sql/Tables/Demo.EggSales.sql
@@ -0,0 +1,14 @@
+﻿if OBJECT_ID(N'Demo.EggSales') Is Null
+Begin
+Create Table Demo.EggSales
+(
+	EggSalesID int Not Null Primary Key,
+	UnitPrice Decimal(10,2) NOt Null,
+	BatchEggID int not null Foreign key
+		References Demo.BatchEgg(BatchEggID),
+	RetailID int not null Foreign Key
+		References Demo.Retail(RetailID),
+	CanceledOn Datetime Null,
+	Unique(BatchEggID, RetailID)
+);
+End
