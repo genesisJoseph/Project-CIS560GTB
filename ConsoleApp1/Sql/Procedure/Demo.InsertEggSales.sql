85f9c9609bf59c88ab1a920d5df8998cb2e03e06
Betzabe Pinon <NikkiPilk@outlook.com>
2026-04-27 14:09:02 -0500

    add lable and grid

---
+++ ConsoleApp1/Sql/Procedure/Demo.InsertEggSales.sql
@@ -0,0 +1,32 @@
+﻿Create or Alter Procedure Demo.CreateEggSales
+	@EggSalesId int output,
+	@UnitPrice Decimal(10,2),
+	@CanceledOn Datetime Null
+As 
+
+Insert Demo.EggSales(UnitPrice, CanceledOn)
+Values(@UnitPrice, @CanceledOn);
+
+Set @EggSalesId = Scope_Identity();
+Go;
+
+SELECT 
+    es.EggSalesID,
+    es.UnitPrice,
+    es.AvgUnit,
+    es.CanceledOn,
+    r.Name AS RetailName,
+    b.BatchEggID
+FROM Demo.EggSales es
+JOIN Demo.Retail r ON es.RetailID = r.RetailID
+JOIN Demo.BatchEgg b ON es.BatchID = b.BatchEggID;
+
+UPDATE Demo.EggSales
+SET UnitPrice = @UnitPrice,
+    CanceledOn = @CanceledOn
+WHERE EggSalesID = @EggSalesID;
+
+
+SELECT * 
+FROM EggSales
+WHERE CanceledOn IS NULL;
---
+++ ConsoleApp1/Sql/Procedure/UpdateRetail.sql
@@ -0,0 +1,22 @@
+﻿Create or Alter Procedure Demo.CreateRetail
+	@RetailId int output,
+	@PhoneNumber NVarChar(20),
+	@Name NvarChar(20),
+	@RetailFee Decimal(5,4)
+As
+
+Insert Demo.Retail(PhoneNumber, [Name], RetailFee)
+Values(@PhoneNumber, @Name, @RetailFee);
+
+Set @RetailId = Scope_Identity();
+Go;
+
+UPDATE Demo.Retail
+SET [Name] = @Name,
+    PhoneNumber = @PhoneNumber,
+    RetailFee = @RetailFee
+WHERE RetailID = @RetailID;
+
+SELECT R.RetailID, R.PhoneNumber, R.[Name], R.RetailFee 
+FROM Demo.Retail R 
+WHERE RetailID = @RetailID;
